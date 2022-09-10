32d6ef0a2d61e990e711c48ab0bcfa7021e6b9be
Alejandro Zorita <alejandrozorita@Alejandros-MacBook-Pro.local>
2022-09-10 19:07:33 +0200

    Cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes')
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middelware/error.handler')
@@ -7,6 +8,19 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whiteListe = ['http://localhost:8080', 'https://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteListe.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('not allowed'));
+    }
+  }
+
+}
+app.use(cors(options));
 
 app.get("/", (req, res) => {
   res.send("Hola mi server express");
